c3f7e6281c5df3459669e12c0548d134f3ab0d76
Harrison Goldstein <hgoldstein95@gmail.com>
2016-09-17 19:37:25 -0400

    Fixed bug in test, interface issue. Passed empty paths through parseReader.

--- src/test/TaskAnalyzerTest.java
+++ src/test/TaskAnalyzerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
@@ -21,7 +22,7 @@ public class TaskAnalyzerTest {
         BufferedReader br = new BufferedReader(new StringReader(test));
         TaskTuple res = null;
         try {
-            res = TaskAnalyzer.parseReader("test.java", br);
+            res = TaskAnalyzer.parseReader("test.java", br, Paths.get(""));
         } catch (IOException e) {
             fail();
         }
@@ -29,11 +30,25 @@ public class TaskAnalyzerTest {
         assertEquals(1, res.todos.size());
     }
 
+    @Test
+    public void testComplex() {
+        String test = "System.out.println(\"// TODO: Don't do that thing\"); // TODO: Do the thing.\n";
+        BufferedReader br = new BufferedReader(new StringReader(test));
+        TaskTuple res = null;
+        try {
+            res = TaskAnalyzer.parseReader("test.java", br, Paths.get(""));
+        } catch (IOException e) {
+            fail();
+        }
+        assertNotNull(res);
+        assertEquals("Do the thing.", res.todos.get(0).message);
+    }
+
     @Test(expected = IOException.class)
     public void testUnrecognizedFiletype() throws IOException {
         String test = "// TODO: Do the thing.";
         BufferedReader br = new BufferedReader(new StringReader(test));
         TaskTuple res = null;
-        res = TaskAnalyzer.parseReader("test.bad", br);
+        res = TaskAnalyzer.parseReader("test.bad", br, Paths.get(""));
     }
 }

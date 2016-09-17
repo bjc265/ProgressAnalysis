4e7f9e94e760c6248efd94f15f247853ab95a0b4
gcg45 <gcg45@cornell.edu>
2016-09-17 17:47:49 -0400

    Working on test

--- src/test/HTMLGeneratorTest.java
+++ src/test/HTMLGeneratorTest.java
@@ -3,6 +3,7 @@ package test;
 import generation.HTMLGenerator;
 import org.junit.*;
 import java.io.File;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertTrue;
 
@@ -14,7 +15,8 @@ public class HTMLGeneratorTest {
 
     @Test
     public void test(){
-        File src_file = new File("src");
+        File src_file = Paths.get("src").toFile();
+        System.out.println("" + Paths.get("src").toFile().isDirectory());
         HTMLGenerator.generateHTML(src_file);
         System.out.println(HTMLGenerator.HTMLText);
     }

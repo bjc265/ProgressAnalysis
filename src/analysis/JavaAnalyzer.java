4cea61a905b897cc47749a806c6c130273a3f0df
bjc265 <bjc265@cornell.edu>
2016-09-17 03:48:55 -0400

    Implement task search in Java files

--- src/analysis/JavaAnalyzer.java
+++ src/analysis/JavaAnalyzer.java
@@ -1,8 +1,14 @@
 package analysis;
 
 import data.SAObject;
-import java.util.HashSet;
+import data.Task;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.ArrayList;
 import java.io.File;
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * Created by brett on 9/17/16.
@@ -10,6 +16,63 @@ import java.io.File;
 public class JavaAnalyzer {
 
     public static SAObject parseFile(File f) {
+
+        String name = null;
+        List<Task> tasks = new ArrayList<>();
+        Set<SAObject> references = new HashSet<>();
+        Set<SAObject> referenced_by = new HashSet<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(f));
+            String line = reader.readLine();
+            int lineNum = 1;
+            while(line != null) {
+                int todoIndex = line.indexOf("//");
+                if (todoIndex > -1) {
+                    String before = line.substring(0, todoIndex);
+                    before.replace("\\\"", "");
+                    int beforeQuot = before.indexOf("\"");
+                    int totalQuots = 0;
+                    while (beforeQuot > -1) {
+                        totalQuots++;
+                        before = before.substring(beforeQuot + 1);
+                        beforeQuot = before.indexOf("\"");
+                    }
+                    if (totalQuots % 2 == 0) {
+                        switch(line.substring(todoIndex+2,todoIndex+7)) {
+                            case "TODO ":
+                                tasks.add(new Task(Task.TaskType.TODO, lineNum,
+                                        line.substring(todoIndex+6)));
+                                break;
+                            case "COMPL":
+                                if(line.substring(todoIndex+2,todoIndex+11).equals("COMPLETE ")) {
+                                    tasks.add(new Task(Task.TaskType.COMPLETE, lineNum,
+                                            line.substring(todoIndex+11)));
+                                    break;
+                                }
+                            case "IN_PR":
+                                if(line.substring(todoIndex+2,todoIndex+13).equals("IN_PROGRESS ")) {
+                                    tasks.add(new Task(Task.TaskType.IN_PROGRESS, lineNum,
+                                            line.substring(todoIndex+13)));
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+
+
+                line = reader.readLine();
+                lineNum++;
+            }
+
+            return new SAObject(name,references,referenced_by,tasks);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
--- src/data/SAObject.java
+++ src/data/SAObject.java
@@ -1,13 +1,23 @@
 package data;
 
 import java.util.Set;
+import java.util.List;
 
 /**
  * Created by brett on 9/17/16.
  */
 public class SAObject {
 
+    private String name;
     private Set<SAObject> references;
     private Set<SAObject> referenced_by;
-    private Set<Task> tasks;
+    private List<Task> tasks;
+
+    public SAObject(String s, Set<SAObject> refs, Set<SAObject> refd, List<Task> t) {
+        name = s;
+        references = refs;
+        referenced_by = refd;
+        tasks = t;
+    }
+
 }
